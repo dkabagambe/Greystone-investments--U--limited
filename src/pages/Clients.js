17e84321f242b40ab16a106f8a849b23329fddd8
daniel Kabagambe <danielkabagambe@gmail.com>
2025-01-31 12:57:16 +0300

    Our Clients

--- src/pages/Clients.js
+++ src/pages/Clients.js
@@ -1,8 +1,7 @@
-import React from "react";
 import "../styles/Clients.css";
 import img1 from "../img/maaif.jpeg";
 import img2 from "../img/lands.jpg";
-import img3 from "../img/water and environment.jpg";
+import img3 from "../img/water and environment.jpeg";
 import img4 from "../img/molg.jpg";
 import img5 from "../img/moe&s.jpg";
 import img6 from "../img/mow&t.png";
@@ -13,20 +12,19 @@ const Clients = () => {
       <h3>Our Clients</h3>
       <div className="clients">
         <div className="card">
+          <img src={img2} alt="clients" />
+        </div>
+        <div className="card">
           <img src={img5} alt="clients" />
         </div>
         <div className="card">
-          <img src={img2} alt="clients" />
+          <img src={img6} alt="clients" />
         </div>
         <div className="card">
           <img src={img3} alt="clients" />
         </div>
-
         <div className="card">
           <img src={img1} alt="clients" />
-        </div>
-        <div className="card">
-          <img src={img6} alt="clients" />
         </div>
         <div className="card">
           <img src={img4} alt="clients" />
